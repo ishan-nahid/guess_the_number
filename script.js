caaa00de2772953287e04bd6eb9a4046997ec67c
Ishan Ahmad <nahidboss96@gmail.com>
2023-10-18 20:21:42 +0600

    added fireworks

--- script.js
+++ script.js
@@ -19,6 +19,34 @@ checkButton.addEventListener("click", function() {
         message.textContent = "Please enter a valid number between 1 and 100.";
     } else if (userGuess === secretNumber) { // Check if the user's guess is correct
         message.textContent = `Congratulations! You guessed the number in ${attempts} attempts.`;
+        // confetti({
+        //     particleCount: 100,
+        //     spread: 70,
+        //     origin: { y: 0.6 }
+        // });
+
+        var duration = 5 * 1000;
+        var animationEnd = Date.now() + duration;
+        var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+        function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+        }
+
+        var interval = setInterval(function() {
+        var timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+            return clearInterval(interval);
+        }
+
+        var particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+        confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+        }, 250);
+
+        
         guessInput.disabled = true; // Disable the input field
         checkButton.disabled = true; // Disable the "Check" button
     } else if (userGuess < secretNumber) { // Check if the guess is too low
